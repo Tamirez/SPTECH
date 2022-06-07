024b2371a8ac49e374de011e1b780b45fbcb9c5b
tamirez <tamirez.melo@sptech.school>
2022-06-07 13:40:13 -0300

    mudanças feitas em 07/06

---
+++ PROJETO PESSOAL/src/database/script-tabelas.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE filme;
+
+USE filme;
+
+CREATE TABLE usuario (
+id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR (45),
+email VARCHAR (45),
+senha VARCHAR (45),
+fkfavorito INT, 
+FOREIGN KEY (fkfavorito) REFERENCES favorito(id_favs)
+) AUTO_INCREMENT = 10;
+
+CREATE TABLE favorito (
+id_favs INT PRIMARY KEY AUTO_INCREMENT,
+diretor_fav VARCHAR (45)
+);
